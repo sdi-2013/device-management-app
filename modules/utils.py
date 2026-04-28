34851268b881546be5ae9b6ed04d11ab15d98e39
sdi-2013 <justice4756@gmail.com>
2026-04-28 19:44:01 +0900

    Add files via upload

--- modules/utils.py
+++ modules/utils.py
@@ -29,3 +29,25 @@ def normalize_id(val):
 def get_unique_id():
     """Generates a UUID string."""
     return str(uuid.uuid4())
+
+def get_chosung(text):
+    """
+    한글 문자열에서 초성만 추출합니다. (예: '피복' -> 'ㅍㅂ')
+    한글이 아닌 문자는 그대로 유지합니다.
+    """
+    if not text:
+        return ""
+    
+    CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    result = []
+    
+    for char in text:
+        code = ord(char)
+        if 0xAC00 <= code <= 0xD7A3:
+            # 한글 음절인 경우 초성 인덱스 계산
+            chosung_index = (code - 0xAC00) // 588
+            result.append(CHOSUNG_LIST[chosung_index])
+        else:
+            result.append(char)
+            
+    return "".join(result)
